d300a1d93abbe032b9c0734b196fa069ce7f2e6c
minescoviciu <andreiminescu@gmail.com>
2018-04-14 17:28:07 +0300

    nou

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,23 +6,37 @@ public class PlayerMovement : MonoBehaviour {
 
 
     public float speed = 1;
+    public float jumpForce = 10;
 
-    Animator animator;
+    Animator anim;
     Rigidbody2D rigidbody2D;
     public BackgroundParallax background;
     bool facingRight = true;
 
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
     const string speedAnimator = "speed";
+    const string groundnAnimator = "isGrounded";
+    const string vSpeedAnimator = "vSpeed";
 
 	// Use this for initialization
 	void Start ()
     {
-        animator = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate  () {
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        anim.SetBool(groundnAnimator, grounded);
+        anim.SetFloat(vSpeedAnimator, rigidbody2D.velocity.y);
+
         float direction = Input.GetAxis("Horizontal") * speed;
         if (facingRight && direction < 0)
         {
@@ -33,10 +47,20 @@ public class PlayerMovement : MonoBehaviour {
             Flip();
         }
 
-        animator.SetFloat(speedAnimator, Mathf.Abs(direction));
-        rigidbody2D.velocity = Vector2.right * direction;
+        anim.SetFloat(speedAnimator, Mathf.Abs(direction));
+        rigidbody2D.velocity = new Vector2(direction, rigidbody2D.velocity.y); //Vector2.right * direction;
         background.updateBackgroundPosition(direction, transform.position);
 
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            anim.SetBool(groundnAnimator, false);
+            rigidbody2D.AddForce(Vector2.up * jumpForce);
+            Debug.Log("Jump");
+        }
     }
 
     void Flip()
