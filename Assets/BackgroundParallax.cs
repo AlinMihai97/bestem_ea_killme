aada7afe9e4ee8cc682756bc5d250ea6fc7ba327
unknown <alinmihai97@gmail.com>
2018-04-14 13:50:03 +0300

    Added temporarry background

---
+++ Assets/BackgroundParallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour {
+
+    static float parallaxFactor = 0.95f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void updateBackgroundPosition(float objectSpeed)
+    {
+        transform.position += new Vector3(objectSpeed * parallaxFactor, 0, 0);
+    }
+}
